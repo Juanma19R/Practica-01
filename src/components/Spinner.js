27ee4b0d3e1c5f6c5cfe824ec84e3b634c92ad0b
Juanma19R <juan.ruggeri@hotmail.com>
2022-06-03 02:18:51 -0300

    Se agrega el proyecto complero al repositorio

---
+++ src/components/Spinner.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+//Styles
+import '../css/Spinner.css';
+
+const Spinner = () => {
+    return(
+        <div class="loadingio-spinner-ellipsis-xwgbwhwqrh">
+            <div class="ldio-py87g1e9w5s">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
